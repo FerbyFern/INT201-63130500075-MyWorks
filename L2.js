12cf16ef3ed71f458a957c36ad3553c8adc31ad8
Panalee <fernsat45@gmail.com>
2021-09-02 15:28:55 +0700

    new

---
+++ L2.js
@@ -0,0 +1,11 @@
+const student={id:1, name:"Somchai"}
+
+for(let prop in student){
+console.log(prop);
+console.log(student[prop])
+}
+
+let data=[1, 10, 5, "Hello", true]
+for(let item of data){
+console.log(item)
+}
