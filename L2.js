c58bbe3081d3646780970adc8ad6dfab4f99dd71
Panalee <fernsat45@gmail.com>
2021-09-02 15:45:27 +0700

    new

--- L2.js
+++ L2.js
@@ -9,3 +9,13 @@ let data=[1, 10, 5, "Hello", true]
 for(let item of data){
 console.log(item)
 }
+
+//obj. literal
+let square ={
+area: function(){ return this.side * this.side;},  
+side: 10  
+};
+square.area()
+
+let book={isbn: 1111, title: "JS Beginner", author:{firstname: "John", lastname: "Hang"}}
+book.versions =[1.0,1.5, 2.0]
