ca3788b1ac17c76f8303cde934c4bc75aa09ef77
Panalee <fernsat45@gmail.com>
2021-09-02 17:44:27 +0700

    up

---
+++ Q5-QAns.js
@@ -0,0 +1,34 @@
+const questionQuiz = [
+    {
+      question : "What is 2+1",
+      answer : {
+          a: '5',
+          b: '4',
+          c: '3'
+     
+     
+      },
+      correctAnswer: 'c'
+    },
+    {
+       question : "What food panda eat",
+      answer : {
+          a: 'bamboo',
+          b: 'ant',
+          c: 'rice'
+     
+     
+      },
+      correctAnswer: 'a'
+
+    }
+
+
+
+
+]
+
+console.log(questionQuiz[0])
+   
+
+
