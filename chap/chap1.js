8d6d9eada88a3efd827b7499811e82c5f4e8cfab
Panalee <fernsat45@gmail.com>
2021-09-15 23:03:37 +0700

    [new]

---
+++ chap/chap1.js
@@ -0,0 +1,12 @@
+console.log(12345)
+console.log('Hello World')
+console.log('One', 'Two', 'Three')
+console.log('สวัสดี ชาวโลก')
+
+var a = 'ECMAScript '
+var b = 8
+console.log(a + b)
+
+var a = 'ECMAScript ';
+var b = 8;
+console.log(a + b);
---
+++ chap/char2.js
@@ -0,0 +1,56 @@
+//ประกาศตัวแปร
+var x
+var result
+let message
+
+var p, q, r
+let num1, num2, result
+
+var a
+let b
+// let a   //error
+// var a   //error
+// var b   //error
+
+//กำหนดค่าตัวแปร
+var text              //ประกาศตัวแปรชื่อ text
+text = ค่าที่กำหนด      //กำหนดค่าให้กับตัวแปร text
+let number
+number = ค่าที่กำหนด
+
+var text = ค่าที่กำหนด        //ประกาศตัวแปรชื่อ text พร้อมกำหนดค่า
+let number = ค่าที่กำหนด      //ประกาศตัวแปรชื่อ number พร้อมกำหนดค่า
+
+var a = ค่าที่กำหนด, b = ค่าที่กำหนด, c = ค่าที่กำหนด   //กำหนดค่าให้กับทุกตัวแปร
+let x, y = ค่าที่กำหนด, z                          //กำหนดค่าเพียงบางตัวแปร
+
+var number = ค่าที่1
+number = ค่าที่2         //เปลี่ยนแปลงค่าได้
+number = ค่าที่3
+
+//ข้อมูลชนิด number
+var x = 123             //เลขจำนวนเต็ม
+var y = -456            //ค่าติดลบ
+let z = 456.789         //เลขทศนิยม
+let a = 108, b = 1009   //อาจประกาศและกำหนดค่าในคำสั่ง vat / let เดียวกัน
+var c = +123            //เมื่อนำไปใช้ จะได้ค่าเป็น 123
+var d = 007             //เมื่อนำไปใช้ จะได้ค่าเป็น 7
+var e = 1.0             //เมื่อนำไปใช้ จะได้ค่าเป็น 1
+
+// var a = 123,456         //error
+// var b = 7_890           //error
+
+var x = 123             //เดิมเป็นจำนวนเต็ม
+x = 4.56                //เปลี่ยนเป็นเลขทศนิยม
+var y = 7.11            //เดิมเป็นเลขทศนิยม
+y = 101                 //เปลี่ยนเป็นจำนวนเต็ม
+
+//ข้อมูลชนิด String
+var d = "JavaScript"
+var s = 'avaScript'
+var b = `JavaScript`
+
+/* var text1 = "Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry"              //error
+   var text1 = 'Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry'              //error   */
