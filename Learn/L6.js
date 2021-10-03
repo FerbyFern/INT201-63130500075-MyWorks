d5c9d1ed5866fbcac3791e7d6540bf56708cb158
Panalee <fernsat45@gmail.com>
2021-10-03 15:22:16 +0700

    mid

--- Learn/L6.js
+++ Learn/L6.js
@@ -28,29 +28,60 @@ console.log(studentSec2);
 
 [instructor, ta, ...student] = ["Umaporn", "Tisanai", "A", "B", "C"];
 console.log(student);
+console.log(student.length);
+student[0] = 'Paramet';
+console.log(student);
 
 /*************************** */
-//1.
+//function expression
 const myFunc = function (instructor, ta, ...students) {
   let greeting = "Hello";
   return students;
 };
 
+/*************************** */
+//แปลงเป็น arrow function
+//1.
+const myFunc = function (instructor, ta, ...students) {
+  let greeting = "Hello";
+  return students;
+};
+//
+const myFunc = (instructor, ta, ...students) => {
+                                                let a = 0
+                                                return student}
+console.log(myFunc(1,2,3))
 //2.
 function echo(text) {
   return text;
 }
-
+//
+const echo = text => text
 //3.
 function sayHi(greeting, name) {
   return `${greeting}, ${name}`;
 }
+//
+const sayHi = (greeting, name) => `${greeting}, ${name}`
+console.log(sayHi('Hello','Run'))
+
 //4.
 function getClass() {
   return `INT201`;
 }
+//
+const getClass = () => `INT201`
+console.log(getClass())
 
 /**************************** */
+//arguments object ไม่ใช่ array
+const myFunc = function () {
+  for (let i = 0; i < arguments.length; i++)
+  console.log(arguments[i])
+}
+
+/**************************** */
+//defult parameter
 const myFunc = function (instructor = "unknown", numsOfStd = 0) {
   console.log(instructor);
   console.log(numsOfStd);
---
+++ Learn/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+const myFunc = function (instructor = "unknown", numsOfStd = 0) {
+  console.log(instructor);
+  console.log(numsOfStd);
+  return instructor;
+};
+
+myFunc(5);
