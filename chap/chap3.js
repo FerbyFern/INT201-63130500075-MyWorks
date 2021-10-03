2a245e67d9db8f773484d05a729d09f2850e1ef3
Panalee <fernsat45@gmail.com>
2021-10-03 18:02:51 +0700

    mid

--- chap/chap3.js
+++ chap/chap3.js
@@ -218,4 +218,34 @@ do {
 ถูกต้อง
 */
 
- 
+/***********************************************************************/
+/*
+console.log(null == false)
+console.log(undefined == false)
+console.log(NaN == false)
+
+console.log(!false)
+console.log(!null)
+console.log(!undefined)
+console.log(!NaN)
+*/
+
+let a
+if (a == undefined) {
+    console.log('Error')
+}
+
+let b
+if (!b) {
+    console.log('Error')
+}
+
+/*
+const prompt = require('prompt-sync')() 
+var year = parseInt(prompt('Enter year (CE) >> '))
+if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+	console.log (year + ' is a leap year')
+} else {
+    console.log (year + ' is not a leap year')
+}
+*/
