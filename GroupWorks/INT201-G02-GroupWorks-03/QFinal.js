14bcbc1814d8fab75fd4a2ea01156dde95513976
Panalee <fernsat45@gmail.com>
2021-11-04 15:13:31 +0700

    G

---
+++ GroupWorks/INT201-G02-GroupWorks-03/QFinal.js
@@ -0,0 +1,11 @@
+//Object Questions
+//สร้างชุดคำถาม 5 คำถาม โดยสร้าง Object ของ questions ทั้งหมด 5 Object โดยแต่ละ object แบ่งเป็น 3 property คือ question (คำถาม) , answer (ตัวเลือกในการตอบคำถาม) โดยจะมี property ย่อยคือ choice a, b, c , correctAnswer(ตัวเลือกของคำตอบที่ถูกต้อง)
+const question1 = {
+    question: "What is the answer of 2+1?",
+    answer: {
+      a: "5",
+      b: "4",
+      c: "3",
+    },
+    correctAnswer: "c",
+  };
